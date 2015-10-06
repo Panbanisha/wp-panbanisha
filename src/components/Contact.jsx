a183736499f0d7478c77d6623d28ee3568552184
Ryo Ikarashi <ryo.5630@gmail.com>
2015-10-07 05:02:31 +0900

    add ajax form request

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,73 @@
 var React = require('react');
 var DocumentTitle = require('react-document-title');
 
+var $ = require('jquery');
+
 module.exports = React.createClass({
+
+  setSubject() {
+    $('input[name="title"]').keyup((e) => {
+      $('input[name="_subject"]').attr('value', $(e.target).val());
+    });
+  },
+
+  sendForm() {
+    var $contactForm = $('.contact-form__form');
+
+    $contactForm.submit((e) => {
+      e.preventDefault();
+      var EMAIL = 'ryo.5630@gmail.com';
+      var jsonData = {};
+      var data = $(e.target).serializeArray();
+      var $overlay = $('.contact-form__form__overlay');
+
+      $.each(data, function() {
+         if (jsonData[this.name]) {
+           if (!jsonData[this.name].push) {
+               jsonData[this.name] = [jsonData[this.name]];
+           }
+           jsonData[this.name].push(this.value || '');
+         } else {
+            jsonData[this.name] = this.value || '';
+         }
+      });
+
+      $.ajax({
+        url: `//formspree.io/${EMAIL}`,
+        method: 'POST',
+        data: jsonData,
+        dataType: 'json',
+        beforeSend: function() {
+          $overlay.fadeIn(500);
+          $overlay.find('.contact-form__form__overlay__message--beforeSend').fadeIn(500);
+        },
+        success: function(data) {
+          $overlay.fadeIn(500)
+          $overlay.find('.contact-form__form__overlay__message--beforeSend').hide();
+          $overlay.find('.contact-form__form__overlay__message--success').fadeIn(500);
+        },
+        error: function(err) {
+          $overlay.fadeIn(500);
+          $overlay.find('.contact-form__form__overlay__message--beforeSend').hide();
+          $overlay.find('.contact-form__form__overlay__message--error').fadeIn(500);
+        }
+      });
+    });
+  },
+
+  componentDidMount() {
+    this.setSubject();
+    this.sendForm();
+  },
+
+  componentDidUpdate() {
+    this.sendForm();
+  },
+
   render() {
 
     var title = "Contact";
+    var ccEmail = 'office@panbanisha.com';
 
     return (
       <DocumentTitle title={`${title} | Panbanisha`}>
@@ -12,38 +75,55 @@ module.exports = React.createClass({
           <section className="contact-form">
             <h1 className="contact-form__title">Contact Form</h1>
             <form action="" className="contact-form__form">
-              <div className="contact-form__form__block">
-                <div className="input">
-                  <label htmlFor="name">Name</label>
-                  <input type="text" name="name" id="name" />
-                </div>
-                <div className="input">
-                  <label htmlFor="company">Company</label>
-                  <input type="text" name="company" id="company" />
-                </div>
-                <div className="input">
-                  <label htmlFor="email">E-mail Address</label>
-                  <input type="email" name="email" id="email" />
-                </div>
-                <div className="input">
-                  <label htmlFor="number">Phone Number</label>
-                  <input type="number" name="number" id="number" />
-                </div>
-              </div>
-              <div className="contact-form__form__block">
-                <div className="input">
-                  <label htmlFor="message">Message</label>
-                  <div className="message__bg">
-                    <div className="message__bg__input">
-                      <label htmlFor="title">Title</label>
-                      <input name="title" id="title" type="text" />
-                    </div>
-                    <textarea name="message" id="message" cols="60" rows="11"></textarea>
+              <div className="contact-form__form__content">
+                <div className="contact-form__form__block">
+                  <div className="input">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" />
+                  </div>
+                  <div className="input">
+                    <label htmlFor="company">Company</label>
+                    <input type="text" name="company" id="company" />
+                  </div>
+                  <div className="input">
+                    <label htmlFor="email">E-mail Address</label>
+                    <input type="email" name="email" id="email" />
+                  </div>
+                  <div className="input">
+                    <label htmlFor="number">Phone Number</label>
+                    <input type="number" name="number" id="number" />
                   </div>
                 </div>
+                <div className="contact-form__form__block">
+                  <div className="input">
+                    <label htmlFor="message">Message</label>
+                    <div className="message__bg">
+                      <div className="message__bg__input">
+                        <label htmlFor="title">Title</label>
+                        <input name="title" id="title" type="text" />
+                        <input type="hidden" name="_subject" value="" />
+                      </div>
+                      <textarea name="message" id="message" cols="60" rows="11"></textarea>
+                    </div>
+                  </div>
+                </div>
+                <div className="contact-form__form__submit-btn">
+                  <input type="submit" value="Send" />
+                </div>
+                <input type="hidden" name="_cc" value={ccEmail} />
+                <input type="text" name="_gotcha" style={{display: 'none'}} />
               </div>
-              <div className="contact-form__form__submit-btn">
-                <input type="submit" value="Send" />
+              <div className="contact-form__form__overlay">
+                <div className="contact-form__form__overlay__message--beforeSend">
+                  <h2>送信しています。</h2>
+                </div>
+                <div className="contact-form__form__overlay__message--success">
+                  <h2>ありがとうございます。</h2>
+                  <p>〜のくらいで返信します。</p>
+                </div><div className="contact-form__form__overlay__message--error">
+                  <h2>エラーが発生しました。</h2>
+                  <p>もう一度ページを読み込んで、再度送信しなおしてください。</p>
+                </div>
               </div>
             </form>
           </section>
