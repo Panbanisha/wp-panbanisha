17c6821bf8629682f22a055c6ac81d2c3bb323b8
Ryo Ikarashi <ryo.5630@gmail.com>
2015-10-26 16:48:20 +0900

    fix member and role capitalize

--- src/components/WorkList.jsx
+++ src/components/WorkList.jsx
@@ -43,7 +43,7 @@ var WorkItem = React.createClass({
               <h2 className="works__item__caption__title">{work.title}</h2>
               <div className="works__item__caption__member">
                 {work.acf.all_member !== "0" || work.acf.all_member !== undefined ?
-                  work.acf.all_member.map((member) => <p key={member.all_member_role}>{member.all_member_role.toUpperCase()}<span>:</span>{member.all_member_name.toUpperCase()}</p>)
+                  work.acf.all_member.map((member) => <p key={member.all_member_role}>{member.all_member_role}<span>:</span>{member.all_member_name}</p>)
                 : ''}
               </div>
               {work.acf.production_team !== '0' ?
